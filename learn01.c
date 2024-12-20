e4e316fa94b9937bc8e910adb18e148c0756dbb1
jsk050431 <jsk050431@gmail.com>
2024-12-20 19:07:47 +0900

    c learn 02

---
+++ learn01.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+    // // 자료형
+    // int a = 1; //정수형 변수
+    // float b = 3.14; //실수형 변수
+    // const int c = 2; //상수
+    // printf("%d %f %d", a,b,c);
+
+
+    // // --------------------------------
+    // // 출력(printf)
+    // int input;
+    // printf("값을 입력하세요: ");
+    // scanf("%d", &input);
+    // printf("입력값: %d\n", input);
+
+
+    // //--------------------------------
+    // // 입력(scanf)
+    // int a, b;
+    // printf("첫번째 값을 입력하세요: ");
+    // scanf("%d", &a); //'&' 붙여야함!
+    // printf("두번째 값을 입력하세요: ");
+    // scanf("%d", &b);
+
+    // printf("%d + %d = %d\n", a, b, a+b);
+    // printf("%d - %d = %d\n", a, b, a-b);
+    // printf("%d x %d = %d\n", a, b, a*b);
+    // // 나눗셈할때는 
+    // float c = a;
+    // float d = b;
+    // printf("%d / %d = %f\n", a, b, c/d);
+
+    // //--------------------------------
+    // int a = 5;
+    // int b = 4;
+    // printf("%d", a/b); //1
+
+    // int a = 5;
+    // int b = 4;
+    // printf("%f", a/b); //0.000000(이렇겐 안되는듯)
+
+    // float a = 5;
+    // float b = 4;
+    // printf("%f", a/b); //1.250000
+
+    // //--------------------------------
+    // // 문자(한글자)
+    // char a = 'A';
+    // printf("%c\n", a); //A
+
+    // char str[256];
+    // scanf("%s", str, sizeof(str)); //문자열은 '&'안해도됨
+    // printf("%s\n", str);
+
+
+    return 0;
+}
---
+++ learn02.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+
+int main()
+{
+    // //++뿔뿔
+    // int a = 10;
+    // a++;
+    // printf("%d\n", a); //11
+    // a++;
+    // printf("%d\n", a); //12
+
+
+    // //--------------------------------
+    // int b = 20;
+    // printf("%d\n", ++b); // 21   ++b: 플러스 먼저 하고 출력
+    // printf("%d\n", b++); // 21   b++: 출력 먼저 하고 플러스
+    // printf("%d\n", b);   // 22
+
+    // // --------------------------------
+    // // for (선언; 조건; 증감) { }
+    // for (int i=0; i<10; i++)
+    // {
+    //     printf("hello world %d\n", i);
+    // }
+
+    // // while (조건) { }
+    // // 조건 먼저 확인 후 실행
+    // int i = 0;
+    // while (i<10)
+    // {
+    //     printf("hello world %d\n", i++);
+    // }
+
+    // // do { } while (조건);
+    // // 실행 먼저 한 후 조건 확인
+    // int i = 0;
+    // do {
+    //     printf("hello world %d\n", i++);
+    // } while (i<10);
+
+
+    // //---------------------------------
+    // // 구구단
+    // for (int i=2; i<=9; i++)
+    // {
+    //     for (int j=1; j<=9; j++)
+    //     {
+    //         printf("%dx%d=%d\n", i,j,i*j);
+    //     }
+    // }
+
+
+    // // 피라미드
+    // printf("숫자입력: ");
+    // int num;
+    // scanf("%d", &num);
+
+    // for (int i=1; i<=num; i++)
+    // {
+    //     for (int j=1; j<=i; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+
+    // //역피라미드
+    // printf("숫자입력: ");
+    // int num;
+    // scanf("%d", &num);
+
+    // for (int i=num; i>0; i--)
+    // {
+    //     for (int j=1; j<=i; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+
+    // //오른쪽피라미드
+    // printf("숫자입력: ");
+    // int num;
+    // scanf("%d", &num);
+    
+    // for (int i=1; i<=num; i++)
+    // {
+    //     for (int j=num-i; j>0; j--)
+    //     {
+    //         printf(" ");
+    //     }
+
+    //     for (int k=1; k<=i; k++)
+    //     {
+    //         printf("*");
+    //     }
+     
+    //     printf("\n");
+    // }
+
+
+    // 가운데피라미드
+    printf("숫자입력: ");
+    int num;
+    scanf("%d", &num);
+
+    for(int i=1; i<=num; i++)
+    {
+        for (int j=1; j<=num-i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k=1; k<=2*i-1; k++)
+        {
+            printf("*");
+        }
+
+        // for (int l=1; l<=num-i; l++ )
+        // {
+        //     printf(" ");
+        // }
+       
+        printf("\n");
+    }
+
+
+
+
+    return 0;
+}
